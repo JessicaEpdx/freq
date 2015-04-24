2f700f7b84e4147334f1e7fb72367dfc93db85c0
Jessica Engel <Jessicaepdx@gmail.com>
2015-04-24 12:04:33 -0700

    look prettier online

--- spec/freq_integration_spec.rb
+++ spec/freq_integration_spec.rb
@@ -6,7 +6,7 @@ describe('the frequency path', {:type => :feature}) do
   it('processes both user entries to see how many times word is in string of words') do
     visit('/')
     fill_in('word_to_count', :with => 'cat')
-    fill_in('given_sentence', :with => 'My cat is the coolest cat.')
+    fill_in('given_sentence', :with => 'My cat is the coolest cat')
     click_button("What's the frequency Kenneth?")
     expect(page).to have_content(2)
   end
--- spec/freq_spec.rb
+++ spec/freq_spec.rb
@@ -3,11 +3,11 @@ require('freq.rb')
 require('pry')
 
 describe('String#freq') do
-  it("return the number of times that a given words appears in a given string of words") do
-    expect(("cat").freq("My cat is the coolest cat.")).to(eq(2))
+  it("returns the number of times that a given words appears in a given string of words") do
+    expect(("cat").freq("My cat is the coolest cat")).to(eq(2))
   end
 
-  it("return the number of times that a given words appears in a given string of words, even if words in string are capitals or attached to punctuation") do
+  it("returns the number of times that a given words appears in a given string of words, even if words in string are capitals or attached to punctuation") do
     expect(("pizza").freq("Pizza is the greatest. Pizza, pizza pizza!")).to(eq(4))
   end
 
